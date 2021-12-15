3d7c7b909d791b94270f8e354cc316fba744d1fb
郭亚龙 <guoyalong>
2021-12-15 16:34:48 +0800

    拉取项目

---
+++ Puglin/build/generated/source/buildConfig/debug/com/gyl/permission/puglin/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.gyl.permission.puglin;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.gyl.permission.puglin";
+  public static final String BUILD_TYPE = "debug";
+  // Field from default config.
+  public static final String SDK_PACKAGE = "com.gyl.permission.control";
+  // Field from default config.
+  public static final String SDK_SHA1 = "07:80:AC:8D:C2:19:89:EB:50:C2:DA:E8:79:EE:F3:51:13:88:07:A2";
+}
